b2619e376bb11bb87405e5a7dc52202fea69b2d9
Tom Willemsen <tomwillemsen1995@gmail.com>
2021-12-02 00:06:29 +0000

    explicit types

--- 1/day1.c
+++ 1/day1.c
@@ -1,16 +1,16 @@
+#include <inttypes.h>
 #include <stdio.h>
-#include <limits.h>
 
 int main(int argc, char** argv) {
-    int v1 = INT_MAX, v2 = INT_MAX, v3 = INT_MAX, v4;
-    int part1 = 0, part2 = 0;
+    uint64_t v1 = UINT64_MAX, v2 = UINT64_MAX, v3 = UINT64_MAX, v4;
+    uint64_t part1 = 0, part2 = 0;
     
     if (argc < 2) return 1;
     
     FILE* fptr = fopen(argv[1], "r");
     if (fptr == NULL) return 2;
     
-    while(fscanf(fptr, "%d\n", &v4) != EOF) {
+    while(fscanf(fptr, "%" SCNu64, &v4) != EOF) {
         if (v3 < v4) part1++;
         if (v1 < v4) part2++;
         v1 = v2;
@@ -20,7 +20,7 @@ int main(int argc, char** argv) {
     
     fclose(fptr);
     
-    printf("Part 1: %d\n", part1);
-    printf("Part 2: %d\n", part2);
+    printf("Part 1: %" PRIu64 "\n", part1);
+    printf("Part 2: %" PRIu64 "\n", part2);
     return 0;
 }
