7e28270ef20a16bf6dab7c9970d7a2c52445e771
Tom Willemsen <tomwillemsen1995@gmail.com>
2021-12-06 13:25:00 +0000

    make faster

--- 6/day6_impl.c
+++ 6/day6_impl.c
@@ -1,7 +1,7 @@
 #include "day6_impl.h"
 
 int64_t simulate(ll_i64* initialstate, int64_t days) {
-    int64_t numbers[MAX_DAYS], result = 0, day, i, resetfish;
+    int64_t numbers[MAX_DAYS], result = 0, day, i;
     
     for(day=0; day<MAX_DAYS; ++day) {
         numbers[day] = 0;
@@ -14,14 +14,14 @@ int64_t simulate(ll_i64* initialstate, int64_t days) {
     }
     
     for(i=0; i<days; ++i) {
-        resetfish = numbers[0];
-        
-        for (day=1; day<MAX_DAYS; ++day) {
-            numbers[day-1] = numbers[day];
-        }
-        
-        numbers[6] += resetfish;
-        numbers[8] = resetfish;
+        // instead of treating numbers as a bin count treat numbers
+        // as a circular list. i.e. effective "starting index" of fish
+        // with 0 ticks left shifts by 1 (modulo MAX_DAYS) each "day".
+        // Then only need to do something about the fish who are on
+        // "tick 0" that day (by incrementing "tick 7" by that amount)
+        // The fish which were in life 0 will get effectively moved to 
+        // the end (i.e. life 9).
+        numbers[(i+REPRO_DAYS)%MAX_DAYS] += numbers[i%MAX_DAYS];
     }
     
     for (day=0; day<MAX_DAYS; ++day) {
--- 6/day6_impl.h
+++ 6/day6_impl.h
@@ -5,6 +5,7 @@
 #include "conversions.h"
 #include "file_loading.h"
 
+static const int64_t REPRO_DAYS = 7;
 static const int64_t MAX_DAYS = 9;
 
 int64_t simulate(ll_i64* initialstate, int64_t days);
