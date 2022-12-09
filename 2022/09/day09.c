6ff22da0acd0ced0d99c81c8377fffe61773a38f
Tom Willemsen <tomwillemsen1995@gmail.com>
2022-12-09 23:58:01 +0000

    remove debug timing

--- 2022/09/day09.c
+++ 2022/09/day09.c
@@ -1,8 +1,6 @@
 #include "day09_impl.h"
-#include <time.h>
 
 int main(int argc, char** argv) {
-    double t = -clock();
     FILE* fptr = open_arg_as_file_or_exit(argc, argv);
     int64_t part1 = 0;
     int64_t part2 = 0;
@@ -16,9 +14,6 @@ int main(int argc, char** argv) {
     
     printf("Part 1: %"PRId64"\n", part1);
     printf("Part 2: %"PRId64"\n", part2);
-    
-    t += clock();
-    printf("%3f ms\n", t/CLOCKS_PER_SEC * 1000);
     
     return EXIT_SUCCESS;
 }
