754b69bcc50e35c4174be56fb0fec4fa6f9cfdec
Tom Willemsen <tomwillemsen1995@gmail.com>
2022-12-15 13:37:38 +0000

    Improve day 15 p2; p1 still terrible

--- 2022/15/day15_impl.c
+++ 2022/15/day15_impl.c
@@ -16,44 +16,23 @@ static inline int64_t is_undetected(int64_t x, int64_t y, list_tuple3_i64* senso
     return 1;
 }
 
-static void parse(char** data, uint64_t data_size, list_tuple3_i64* sensor_distances, set_tuple_i64* beacons, set_tuple_i64* sensors) {
+static void parse(char** data, uint64_t data_size, list_tuple3_i64* sensor_distances, set_tuple_i64* beacons) {
     int64_t sensor_x, sensor_y, beacon_x, beacon_y;
     for (uint64_t i=0; i<data_size; ++i) {
         sscanf(data[i], "Sensor at x=%"SCNd64", y=%"SCNd64": closest beacon is at x=%"SCNd64", y=%"SCNd64, &sensor_x, &sensor_y, &beacon_x, &beacon_y);
         list_tuple3_i64_push_back(sensor_distances, sensor_x, sensor_y, i64abs(sensor_x - beacon_x) + i64abs(sensor_y - beacon_y));
         set_tuple_i64_add(beacons, beacon_x, beacon_y);
-        set_tuple_i64_add(sensors, sensor_x, sensor_y);
     }
-}
-
-int64_t score_undetected_along_edge(
-    int64_t start_x, 
-    int64_t start_y, 
-    int64_t distance, 
-    int64_t x_increment, 
-    int64_t y_increment, 
-    list_tuple3_i64* sensor_distances, 
-    int64_t param2
-) {
-    int64_t x = start_x, y = start_y;
-    for (int64_t i=0; i<distance; ++i) {
-        if (is_undetected(x, y, sensor_distances, 0, 0, param2, param2)) {
-            return x * 4000000 + y;
-        }
-        x += x_increment;
-        y += y_increment;
-    }
-    return -1;
 }
 
 void calculate(char** data, uint64_t data_size, int64_t* part1, int64_t* part2, int64_t param1, int64_t param2) {
     int64_t min_x = INT64_MAX, max_x = INT64_MIN;
+    int64_t x, y;
     
     list_tuple3_i64* sensor_distances = list_tuple3_i64_init(32);
     set_tuple_i64* beacons = set_tuple_i64_init(32);
-    set_tuple_i64* sensors = set_tuple_i64_init(32);
     
-    parse(data, data_size, sensor_distances, beacons, sensors);
+    parse(data, data_size, sensor_distances, beacons);
     
     for (uint64_t i=0; i<list_tuple3_i64_size(sensor_distances); ++i) {
         int64_t sensor_x, sensor_y, distance;
@@ -66,39 +45,58 @@ void calculate(char** data, uint64_t data_size, int64_t* part1, int64_t* part2, 
     for (int64_t x=min_x; x<=max_x; ++x) {
         if (!is_undetected(x, param1, sensor_distances, INT64_MIN, INT64_MIN, INT64_MAX, INT64_MAX) 
             && !set_tuple_i64_contains(beacons, x, param1)
-            //&& !set_tuple_i64_contains(sensors, x, param1)
         ) {
             (*part1)++;
         }
     }
     
     set_tuple_i64_free(beacons);
-    set_tuple_i64_free(sensors);
+    
+    list_tuple_i64* line_segments = list_tuple_i64_init(256);
+    set_tuple_i64* intersection_points = set_tuple_i64_init(256);
     
     for (uint64_t i=0; i<list_tuple3_i64_size(sensor_distances); ++i) {
         int64_t x, y, dist;
         list_tuple3_i64_get(sensor_distances, i, &x, &y, &dist);
         
-        *part2 = score_undetected_along_edge(x-dist-1, y, dist/2, 1, 1, sensor_distances, param2);
-        if (*part2 != -1) {
-            break;
-        }
+        // y = mx + c , store m & c
+        list_tuple_i64_push_back(line_segments, 1, y-x+dist+1);
+        list_tuple_i64_push_back(line_segments, 1, y-x-dist-1);
+        list_tuple_i64_push_back(line_segments, -1, y+x+dist+1);
+        list_tuple_i64_push_back(line_segments, -1, y+x-dist-1);
+    }
+    
+    for (uint64_t i=0; i<list_tuple_i64_size(line_segments); ++i) { 
+        int64_t m1, c1, m2, c2;
+        list_tuple_i64_get(line_segments, i, &m1, &c1);
         
-        *part2 = score_undetected_along_edge(x, y+dist+1, dist/2, 1, -1, sensor_distances, param2);
-        if (*part2 != -1) {
-            break;
+        for (uint64_t j=i; j<list_tuple_i64_size(line_segments); ++j) {
+            list_tuple_i64_get(line_segments, j, &m2, &c2);
+            
+            if (m1 == m2) {
+                // parallel lines, won't intersect
+                continue;
+            }
+            
+            int64_t px = (c2-c1)/(m1-m2);
+            int64_t py = (m1*(c2-c1))/(m1-m2) + c1;
+            if (px >= 0 && px <= param2 && py >= 0 && py <= param2) {
+                set_tuple_i64_add(intersection_points, px, py);
+            }
         }
-        
-        *part2 = score_undetected_along_edge(x, y-dist-1, dist/2, 1, -1, sensor_distances, param2);
-        if (*part2 != -1) {
-            break;
-        }
-        
-        *part2 = score_undetected_along_edge(x+dist+1, y, dist/2, 1, -1, sensor_distances, param2);
-        if (*part2 != -1) {
+    }
+    
+    set_tuple_i64_iterator* iter = set_tuple_i64_iter(intersection_points);
+    while (set_tuple_i64_next(iter, &x, &y)) {
+        if (is_undetected(x, y, sensor_distances, 0, 0, param2, param2)) {
+            *part2 = 4000000 * x + y;
             break;
         }
     }
+    set_tuple_i64_iter_free(iter);
+    
+    list_tuple_i64_free(line_segments);
+    set_tuple_i64_free(intersection_points);
     
     list_tuple3_i64_free(sensor_distances);
 }
