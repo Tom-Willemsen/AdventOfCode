54d9c46c6d643c9b28371fbbca4399f2818e4444
Tom Willemsen <tomwillemsen1995@gmail.com>
2022-12-20 12:19:27 +0000

    Revert "refactor?"
    This reverts commit 401afdb01d3e0c858a5fda9eac61424b8fdcc40b.

--- 2022/20/day20_impl.c
+++ 2022/20/day20_impl.c
@@ -9,23 +9,6 @@ struct circularlist_node {
 };
 
 
-static void free_circular_list(struct circularlist_node* first) {
-    struct circularlist_node* node = first;
-    struct circularlist_node* tmp;
-    while (node != NULL) {
-        if (node->prev != NULL) {
-            node->prev->next = NULL;
-        }
-        if (node->next != NULL) {
-            node->next->prev = NULL;
-        }
-        tmp = node->next;
-        free(node);
-        node = tmp;
-    }
-}
-
-
 static int64_t score(uint64_t data_size, struct circularlist_node* first) {
     int64_t result = 0;
     struct circularlist_node* node;
@@ -42,6 +25,7 @@ static int64_t score(uint64_t data_size, struct circularlist_node* first) {
         for (uint64_t j=0; j<new_distance; ++j) {
             node = node->next;
         }
+        printf("result from %ld %ld\n", node->original_index, node->value);
         result += node->value;
     }
     
@@ -60,6 +44,7 @@ static void shuffle(uint64_t data_size, struct circularlist_node* first, uint64_
             }
             
             int64_t new_distance = i64modulo_positive(node->value, data_size-1);
+            printf("new distance: %ld (i: %ld)\n", new_distance, i);
             if (new_distance > 0) {
                 moved_node = node;
                 
@@ -80,12 +65,20 @@ static void shuffle(uint64_t data_size, struct circularlist_node* first, uint64_
                 node->next = moved_node;
                 moved_node->prev = node;
             }
+            
+            puts("list...");
+            for (uint64_t j=0; j<data_size; ++j) {
+                printf("List item: original_index %ld value %ld\n", node->original_index, node->value);
+                node = node->next;
+            }
+            puts("< end list >");
         }
     }
 }
 
 
-struct circularlist_node* parse(char** data, uint64_t data_size, int64_t multiplier) {
+void calculate(char** data, uint64_t data_size, int64_t* part1, int64_t* part2) {
+    
     struct circularlist_node* first = NULL;
     struct circularlist_node* prev = NULL;
     struct circularlist_node* node = NULL;
@@ -96,7 +89,7 @@ struct circularlist_node* parse(char** data, uint64_t data_size, int64_t multipl
         node = calloc(1, sizeof(struct circularlist_node));
         
         node->original_index = i;
-        node->value = n * multiplier;
+        node->value = n;
         
         
         if (prev != NULL) {
@@ -110,22 +103,34 @@ struct circularlist_node* parse(char** data, uint64_t data_size, int64_t multipl
     
     node->next = first;
     first->prev = node;
-    return first;
-}
-
-
-void calculate(char** data, uint64_t data_size, int64_t* part1, int64_t* part2) {
     
-    struct circularlist_node* first;
-    
-    first = parse(data, data_size, 1);
     shuffle(data_size, first, 1);
     *part1 = score(data_size, first);
-    free_circular_list(first);
     
-    first = parse(data, data_size, 811589153);
+    node = NULL;
+    prev = NULL;
+    first = NULL;
+    
+    for (uint64_t i=0; i<data_size; ++i) {
+        int64_t n = strtol(data[i], NULL, 10);
+        
+        node = calloc(1, sizeof(struct circularlist_node));
+        
+        node->original_index = i;
+        node->value = n * 811589153;
+        
+        if (prev != NULL) {
+            node->prev = prev;
+            prev->next = node;
+        } else {
+            first = node;
+        }
+        prev = node;
+    }
+    
+    node->next = first;
+    first->prev = node;
+    
     shuffle(data_size, first, 10);
     *part2 = score(data_size, first);
-    free_circular_list(first);
-    
 }
