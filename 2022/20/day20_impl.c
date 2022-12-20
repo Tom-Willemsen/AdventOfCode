25081ebd4670bf5693c3ce123173e773acea4f34
Tom Willemsen <tomwillemsen1995@gmail.com>
2022-12-20 12:03:53 +0000

    Start refactoring

--- 2022/20/day20_impl.c
+++ 2022/20/day20_impl.c
@@ -1,70 +1,136 @@
 #include "day20_impl.h"
 
 
-static int64_t score(list_tuple_i64* data) {
-    int64_t zero_index = INT64_MIN;
+struct circularlist_node {
+    struct circularlist_node* next;
+    struct circularlist_node* prev;
+    int64_t original_index;
+    int64_t value;
+};
+
+
+static int64_t score(uint64_t data_size, struct circularlist_node* first) {
     int64_t result = 0;
+    struct circularlist_node* node;
     
-    for (uint64_t j=0; j<list_tuple_i64_size(data); ++j) {
-        int64_t x, y;
-        list_tuple_i64_get(data, j, &x, &y);
-        if (y == 0) {
-            zero_index = j;
-            break;
+    node = first;
+    
+    while (node->value != 0) {
+        node = node->next;
+    }
+    
+    for(uint64_t i=1000; i<=3000; i+=1000) {
+        int64_t new_distance = i64modulo_positive(1000, data_size);
+            
+        for (uint64_t j=0; j<new_distance; ++j) {
+            node = node->next;
         }
+        printf("result from %ld %ld\n", node->original_index, node->value);
+        result += node->value;
     }
     
-    for(uint64_t j=1000; j<=3000; j+=1000) {
-        int64_t x, y;
-        list_tuple_i64_get(data, (zero_index + j) % list_tuple_i64_size(data), &x, &y);
-        result += y;
-    }
     return result;
 }
 
 
-static void shuffle(list_tuple_i64* data, uint64_t rounds) {
+static void shuffle(uint64_t data_size, struct circularlist_node* first, uint64_t rounds) {
+    struct circularlist_node* node = first;
+    struct circularlist_node* moved_node;
     for (uint64_t r=0; r<rounds; ++r) {
-        for (uint64_t i=0; i<list_tuple_i64_size(data); ++i) {
-            int64_t current_index, dist, index, value;
-            for (uint64_t j=0; j<list_tuple_i64_size(data); ++j) {
-                list_tuple_i64_get(data, j, &current_index, &dist);
-                if (current_index == i) {
-                    current_index = j;
-                    break;
-                }
+        for (uint64_t i=0; i<data_size; ++i) {
+
+            while (node->original_index != i) {
+                node = node->next;
             }
             
-            int64_t new_index = i64modulo_positive(current_index + dist, list_tuple_i64_size(data) - 1);
+            int64_t new_distance = i64modulo_positive(node->value, data_size-1);
+            printf("new distance: %ld (i: %ld)\n", new_distance, i);
+            if (new_distance > 0) {
+                moved_node = node;
+                
+                // Unlink moved node from current neighbours
+                moved_node->prev->next = moved_node->next;
+                moved_node->next->prev = moved_node->prev;
+                
+                moved_node->prev = NULL;
+                
+                for (uint64_t j=0; j<new_distance; ++j) {
+                    node = node->next;
+                }
+                
+                moved_node->next = NULL;
+                
+                node->next->prev = moved_node;
+                moved_node->next = node->next;
+                node->next = moved_node;
+                moved_node->prev = node;
+            }
             
-            list_tuple_i64_get(data, current_index, &index, &value);
-            list_tuple_i64_remove(data, current_index);
-            list_tuple_i64_insert(data, new_index, index, value);
+            puts("list...");
+            for (uint64_t j=0; j<data_size; ++j) {
+                printf("List item: original_index %ld value %ld\n", node->original_index, node->value);
+                node = node->next;
+            }
+            puts("< end list >");
         }
     }
 }
 
 
 void calculate(char** data, uint64_t data_size, int64_t* part1, int64_t* part2) {
-
-    *part1 = 0;
-    *part2 = 1;
     
-    list_tuple_i64* part1_list = list_tuple_i64_init(data_size);
-    list_tuple_i64* part2_list = list_tuple_i64_init(data_size);
+    struct circularlist_node* first = NULL;
+    struct circularlist_node* prev = NULL;
+    struct circularlist_node* node = NULL;
     
     for (uint64_t i=0; i<data_size; ++i) {
         int64_t n = strtol(data[i], NULL, 10);
-        list_tuple_i64_push_back(part1_list, i, n);
-        list_tuple_i64_push_back(part2_list, i, n * 811589153);
+        
+        node = calloc(1, sizeof(struct circularlist_node));
+        
+        node->original_index = i;
+        node->value = n;
+        
+        
+        if (prev != NULL) {
+            node->prev = prev;
+            prev->next = node;
+        } else {
+            first = node;
+        }
+        prev = node;
     }
     
-    shuffle(part1_list, 1);
-    shuffle(part2_list, 10);
+    node->next = first;
+    first->prev = node;
     
-    *part1 = score(part1_list);
-    *part2 = score(part2_list);
+    shuffle(data_size, first, 1);
+    *part1 = score(data_size, first);
     
-    list_tuple_i64_free(part1_list);
-    list_tuple_i64_free(part2_list);
+    node = NULL;
+    prev = NULL;
+    first = NULL;
+    
+    for (uint64_t i=0; i<data_size; ++i) {
+        int64_t n = strtol(data[i], NULL, 10);
+        
+        node = calloc(1, sizeof(struct circularlist_node));
+        
+        node->original_index = i;
+        node->value = n * 811589153;
+        
+        if (prev != NULL) {
+            node->prev = prev;
+            prev->next = node;
+        } else {
+            first = node;
+        }
+        prev = node;
+    }
+    
+    node->next = first;
+    first->prev = node;
+    
+    shuffle(data_size, first, 10);
+    *part2 = score(data_size, first);
 }
