a681ae357f195181ded454eab6bff978e9c6930b
Tom Willemsen <tomwillemsen1995@gmail.com>
2022-12-23 14:15:28 +0000

    Add non-general Day22 solution

---
+++ 2022/22/day22_impl.h
@@ -0,0 +1,13 @@
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "file_loading.h"
+#include "deque.h"
+#include "map.h"
+#include "list_tuple3.h"
+#include "set_tuple2.h"
+#include "util.h"
+
+void calculate(char** data, uint64_t data_size, int64_t* part1, int64_t* part2);
